6f6a98d609b91be7f7bdabf6544b9544ed166b6f
Shailendra Yadav <shailendra@bottle.com.np>
2019-10-17 17:07:14 +0545

    deployment readme update

--- service_scripts/push.sh
+++ service_scripts/push.sh
@@ -1,3 +1,25 @@
-$(aws ecr get-login --region us-west-2 --no-include-email)
-docker tag ecs-node-react-ecr:latest 713147272639.dkr.ecr.us-west-2.amazonaws.com/shailendra-ecr-node-react-ecr:latest
-docker push 713147272639.dkr.ecr.us-west-2.amazonaws.com/shailendra-ecr-node-react-ecr:latest
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            profile)     PROFILE=${VALUE} ;; 
+            region)       REGION=${VALUE} ;;   
+            localImage)   LOCAL_IMAGE=${VALUE} ;;
+            ecrImage)     ECR_IMAGE=${VALUE} ;;
+            *)   
+    esac    
+
+
+done
+
+echo "REGION = $REGION"
+echo "LOCAL_IMAGE = $LOCAL_IMAGE"
+echo "ECR_IMAGE = $ECR_IMAGE"
+echo "PROFILE = $PROFILE"
+
+$(aws ecr get-login --region $REGION --no-include-email --profile=$PROFILE)
+docker tag $LOCAL_IMAGE:latest $ECR_IMAGE:latest
+docker push $ECR_IMAGE:latest
